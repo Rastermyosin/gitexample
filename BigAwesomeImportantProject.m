0b82137d7fc7714f2f155e811a61dc70f2b992b6
Kevin O'Neill <kjon.oneill@gmail.com>
2012-05-14 09:32:37 -0600

    First start of the reall awesome project!

---
+++ BigAwesomeImportantProject.m
@@ -0,0 +1,8 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%   Title:      The Really Big Awesome Important Project    
+%   Desc:           -Will Revolutionize Mankind
+%   Author:     Kevin O'Neill
+%   Version:    1.0
+%   Date:       5/14/2012
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
