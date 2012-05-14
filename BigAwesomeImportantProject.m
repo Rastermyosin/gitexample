a241c107fb0c1532fa799adb73cd0aa3b773ac69
Kevin O'Neill <kjon.oneill@gmail.com>
2012-05-14 10:01:02 -0600

    Updated version number

--- BigAwesomeImportantProject.m
+++ BigAwesomeImportantProject.m
@@ -2,7 +2,7 @@
 %   Title:      The Really Big Awesome Important Project    
 %   Desc:           -Will Revolutionize Mankind
 %   Author:     Kevin O'Neill
-%   Version:    2.5
+%   Version:    3.0
 %   Date:       5/14/2012
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
