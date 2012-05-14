bd6dd1cfda6e3d9055773c47b2014efd64732319
Kevin O'Neill <kjon.oneill@gmail.com>
2012-05-14 09:44:09 -0600

    Altered the code to catch errors in use input. Need to branch for the magicFoo function.

--- BigAwesomeImportantProject.m
+++ BigAwesomeImportantProject.m
@@ -32,9 +32,24 @@ function struct = userInput(struct)
 % Requests an input from the user as a number. Converts that sring to a
 %   number.
 
-struct.inputStr = input('Chose a starting positive integer: ', 's');
-    
-struct.inputNum = str2double(struct.inputStr);    
+% Creates a boolean flag for exiting the loop.
+flagTmp = false;
 
+while ~flagTmp
+    struct.inputStr = input('Chose a starting positive integer: ', 's');
+    
+    struct.inputStr = str2double(struct.inputStr);    
+    [sizeRow sizeCol] = size(struct.inputStr);
+    
+    % Check if struct.inputStr is empty or is an array
+    if isempty(struct.inputStr) || sizeRow > 1 || sizeCol > 1 
+        fprintf('You need to input a positive integer!\n');
+    elseif ~(struct.inputStr < 0)% Check if struct.inputStr is a positive integer
+        flagTmp = true;
+        struct.inputNum = uint16(struct.inputStr);   
+    else % All other cases  
+        fprintf('You need to input a positive integer!\n');
+    end
+end
 
 end
