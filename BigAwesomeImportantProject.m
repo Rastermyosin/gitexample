9e95f3420c6184e7824e96f958fe18d614d95782
Kevin O'Neill <kjon.oneill@gmail.com>
2012-05-21 09:31:30 -0600

    Testing

--- BigAwesomeImportantProject.m
+++ BigAwesomeImportantProject.m
@@ -1,6 +1,5 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %   Title:      The Really Big Awesome Important Project    
-%   Desc:           -Will Revolutionize Mankind
 %   Author:     Kevin O'Neill
 %   Version:    4.0
 %   Date:       5/14/2012
